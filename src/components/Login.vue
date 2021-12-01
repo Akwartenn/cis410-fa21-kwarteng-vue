5806be846c8186c995f036ab04b4642856ff0237
Alex <akwarten@rams.colostae.edu>
2021-12-01 13:11:02 -0700

    13.2 11/18 axios, signup

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1>Log in</h1>
+      <div v-if="this.$route.query.signupsuccess" class="alert alert-success">Thanks for signing up, please log in now.</div>
   </div>
 </template>
 
