09dc7b2de5eca5490e2602820781b48cf0a56253
Alex <akwarten@rams.colostae.edu>
2021-12-02 16:26:33 -0700

    15.1 11/30 account

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Kwarteng Reviews</router-link>
+      <router-link class="navbar-brand" to="/">Hotel Reviews</router-link>
       <button
         class="navbar-toggler"
         type="button"
