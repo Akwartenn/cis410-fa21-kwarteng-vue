b57fd639cd1bfe101cd788c635b035b125c16664
Alex <akwarten@rams.colostae.edu>
2021-12-14 21:42:59 -0700

    Home Page design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
     <h1>Home</h1>
+    <h4 style="text-align: center;">Hotels Available to Review</h4>
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Hotel</th>
+      <th scope="col">Hotel Star</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Courtyard by Marriott Denver Cherry Creek </td>
+      <td>&#11088; &#11088; &#11088;</td>
+    </tr>
+    <tr>
+      <td>La Quinta Inn by Wyndham Denver Cherry Creek</td>
+      <td>&#11088; &#11088; </td>
+    </tr>
+    <tr>
+      <td>Extended Stay America</td>
+      <td>&#11088; &#11088; </td>
+    </tr>
+     <tr>
+      <td>Aloft Denver Downtown</td>
+      <td>&#11088; &#11088; &#11088; &#11088; &#11088;</td>
+    </tr>
+     <tr>
+      <td>Denver Hilton City Center</td>
+      <td>&#11088; &#11088; &#11088; &#11088; &#11088;</td>
+    </tr>
+  </tbody>
+</table>
   </div>
 </template>
 
